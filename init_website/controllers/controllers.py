85a859d5dbcb1638ceef434c1a5108d3086bb812
Ernesto Jordan Borjas <jordan.ernesto@gmail.com>
2025-12-10 10:31:17 -0600

    [REM] remove unused modules

--- init_website/controllers/controllers.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import http
-
-
-class InitWebsite(http.Controller):
-    @http.route('/mi_pagina', auth='public', website=True)
-    def mostrar_pagina(self, **kw):
-        return http.request.render('init_website.mi_plantilla', {})
